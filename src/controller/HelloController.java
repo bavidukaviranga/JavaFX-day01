8d37776dd5ae5b9ca1e90e15ad06b63194dd01b3
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-05-15 13:37:17 +0530

    get & set data

--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -2,13 +2,17 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController  {
     public Label lblTitle;
 
+    public TextField txtInput;
 
 
     public void btnClickOnAction(ActionEvent actionEvent) {
-      lblTitle.setText("Welcom to Party..! ");
+        txtInput.getText();
+
+        lblTitle.setText(txtInput.getText());
     }
 }
